f8fed341f98b6be220abdfc4c65534c81d0b3bfc
GigiaJ <gigiaboone@yahoo.com>
2021-04-25 18:28:22 -0400

    Added unit tests for dialougeloader

--- src/test/java/TextTranslator/DialogueLoaderTest.java
+++ src/test/java/TextTranslator/DialogueLoaderTest.java
@@ -1,13 +1,38 @@
 package TextTranslator;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class DialogueLoaderTest {
-    /**
-     * Perhaps break apart the method even to test more efficiently
-     */
-    @Test
-    public void testGetDialogue() {
+public class DialogueLoaderTest extends DialogueLoader {
+    final String commandLine = "tellraw @a[score_DialogueTrigger_min=10,score_DialogueTrigger=10,tag=!Dialogue10,score_TalkTime_min=12,score_TalkTime=12] {\"text\":\"<...> That PokÃ©mon you\\u2019ve got there looks pretty happy. You must be a good Trainer.\"}";
 
+    @Test
+    public void testFindText() {
+        String expected = "<...> That PokÃ©mon you\\u2019ve got there looks pretty happy. You must be a good Trainer.";
+        Assert.assertEquals(expected, findText(commandLine));
+    }
+
+    @Test
+    public void testFindTriggerTag() {
+        Assert.assertTrue(findTriggerTag(commandLine) != -1);
+    }
+
+    @Test
+    public void testFindTalkTime() {
+        Assert.assertTrue(findTalkTime(commandLine) != -1);
+    }
+
+    @Test
+    public void testFindSpeaker() {
+        final String speakerLine = "<CharacterName>:Hi this is a test phrase";
+        final String expected = "CharacterName";
+        Assert.assertEquals(expected, findSpeaker(speakerLine));
+    }
+
+    @Test
+    public void testRemoveSpeaker() {
+        final String speakerLine = "<CharacterName>:Hi this is a test phrase";
+        final String expected = "";
+        Assert.assertEquals(expected, removeSpeaker(speakerLine));
     }
 }
