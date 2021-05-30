1122996e35f57dc3ff35869dbd191efb189e4e1a
GigiaJ <gigiaboone@yahoo.com>
2021-05-30 18:59:46 -0400

    Updated the unit tests to use a local instance of command instead of extending

--- src/test/java/TextTranslator/scene/command/CommandTest.java
+++ src/test/java/TextTranslator/scene/command/CommandTest.java
@@ -3,11 +3,7 @@ package TextTranslator.scene.command;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class CommandTest extends Command {
-
-    public CommandTest() {
-        super("", "", "", 0, 0, 0, 0);
-    }
+public class CommandTest {
 
     @Test
     public void testToCommandForm() {
@@ -36,11 +32,10 @@ public class CommandTest extends Command {
 
     @Test
     public void testGenerateSpeakerEntries() {
-        //String entry = "{\"text\":\"<\"},{\"text\":\"Sina\",\"color\":\"red\"},{\"text\":\"> You there!\"}";
-        //Generate command here?
-        String[] expected = {"{\"text\":\"<\"}", "{\"text\":\"Sina\",\"color\":\"red\"}", "{\"text\":\"> You there!\"}"};
+        Command command = generateCommand();
+        String[] expected = {"{\"text\":\"<\"}", "{\"text\":\"Mom\",\"color\":\"red\"}", "{\"text\":\"> This is a string.\"}"};
         for (int i = 0; i < expected.length; i++) {
-            Assert.assertEquals(expected[i], generateSpeakerEntries()[i]);
+            Assert.assertEquals(expected[i], command.generateSpeakerEntries()[i]);
         }
     }
 
@@ -51,26 +46,30 @@ public class CommandTest extends Command {
 
     @Test
     public void testGenerateTextEntry() {
+        Command command = generateCommand();
         String expected = "\"text\":\"test\"";
-        Assert.assertEquals(expected, generateTextEntry("test"));
+        Assert.assertEquals(expected, command.generateTextEntry("test"));
     }
 
     @Test
     public void testGenerateColorField() {
-        String expected = "\"color:" + this.getColor() + "\"";
-        Assert.assertEquals(expected, generateColorField());
+        Command command = generateCommand();
+        String expected = "\"color:" + command.getColor() + "\"";
+        Assert.assertEquals(expected, command.generateColorField());
     }
 
     @Test
     public void testGetTextFieldKey() {
+        Command command = generateCommand();
         String expected = "\"text\"";
-        Assert.assertEquals(expected, getTextFieldKey());
+        Assert.assertEquals(expected, command.getTextFieldKey());
     }
 
     @Test
     public void testGetTextFieldValue() {
+        Command command = generateCommand();
         String expected = "\"test\"";
-        Assert.assertEquals(expected, getTextFieldValue("test"));
+        Assert.assertEquals(expected, command.getTextFieldValue("test"));
     }
 
     @Test
@@ -86,9 +85,15 @@ public class CommandTest extends Command {
 
     @Test
     public void testWrapWithFieldIdentifier() {
+        Command command = generateCommand();
         String expected = "\"test\"";
-        Assert.assertEquals(expected, wrapWithFieldIdentifier("test"));
+        Assert.assertEquals(expected, command.wrapWithFieldIdentifier("test"));
     }
 
 
+    private Command generateCommand() {
+        return new Command("Mom", "This is a string.", "red", "dialogue1", 1, 1, 1, 1, 1,
+                "tellraw @a[score_DialogueTrigger_min=1,score_DialogueTrigger=1,tag=!Dialogue1,score_TalkTime_min=1,score_TalkTime=1] " +
+                        "[\"\",{\"text\":\"<\"},{\"text\":\"Mom\",\"color\":\"red\"},{\"text\":\"> This is a string.\"}]");
+    }
 }
