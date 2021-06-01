d945f58d2dfc9ea4e6fe9f7c118c32d064321e7c
GigiaJ <gigiaboone@yahoo.com>
2021-05-31 23:00:08 -0400

    More unit test stuff

--- src/test/java/TextTranslator/scene/command/CommandSceneTest.java
+++ src/test/java/TextTranslator/scene/command/CommandSceneTest.java
@@ -3,9 +3,17 @@ package TextTranslator.scene.command;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommandSceneTest {
     @Test
     public void testGetText() {
-        Assert.fail();
+        ArrayList<String> expectedStrings = new ArrayList<String>(Arrays.asList(
+                "tellraw @a[score_DialogueTrigger_min=1,score_DialogueTrigger=1,tag=!Dialogue1,score_TalkTime_min=1,score_TalkTime=1]  [\"\",{\"text\":\"<\"},{\"text\":\"Test\",\"color\":\"red\"},{\"text\":\">}, {\"text\":\"This\"}]",
+                "tellraw @a[score_DialogueTrigger_min=1,score_DialogueTrigger=1,tag=!Dialogue1,score_TalkTime_min=2,score_TalkTime=2]  [\"\",{\"text\":\"<\"},{\"text\":\"Test\",\"color\":\"red\"},{\"text\":\">}, {\"text\":\"test\"}]"));
+        for (int i = 0; i < expectedStrings.size(); i++) {
+            Assert.assertEquals(expectedStrings.get(i), //CommandArrayEntry//);
+        }
     }
 }
